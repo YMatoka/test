52fa47038687822d5f6adc232aa0eb36c8f44e41
Yann Matoka <yann.matoka@yahoo.fr>
2018-04-11 12:14:40 +0200

    ajout d'un oublie

--- index.php
+++ index.php
@@ -2,11 +2,9 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <h1>Page d'acceuil</h1>
-    <title>
-      <h1>Document</h1>
 
-    </title>
+    <title>Document</title>
+    <h1>Page d'acceuil</h1>
   </head>
   <body>
 
